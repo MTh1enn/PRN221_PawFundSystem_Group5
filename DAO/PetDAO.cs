955daf6a6941bf1d373d8e0537c4dc03e391ff93
NET_10_VuHuuHuy <huyvhse173535@fpt.edu.vn>
2024-11-11 10:49:00 +0700

    fix

--- DAO/PetDAO.cs
+++ DAO/PetDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessObjects.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,54 @@ using System.Threading.Tasks;
 
 namespace DAO
 {
-    internal class PetDAO
+    public class PetDAO
     {
+        private PawFundContext context;
+        private static PetDAO instance = null;
+
+        public static PetDAO Instance {  
+            get 
+            { 
+                if(instance == null)
+                {
+                    instance= new PetDAO();
+                } 
+                return instance; 
+            }
+        }
+        public PetDAO()
+        {
+            context = new PawFundContext();
+        }
+
+        public List<Pet> GetPets()
+        {
+
+            return context.Pets.ToList();
+        }
+        public Pet GetPetById(int id)
+        {
+            return context.Pets.SingleOrDefault(m => m.Id.Equals(id));
+        }
+        public bool AddPet(Pet pet)
+        {
+            bool result = false;
+            Pet petExisted = GetPetById(pet.Id);
+            try
+            {
+                if (petExisted == null)
+                {
+                    context.Pets.Add(pet);
+                    context.SaveChanges();
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log
+            }
+            return result;
+        }
+       
     }
 }
